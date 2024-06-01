748c66a9c53b048bbadd16a96938b2391f9c0d73
lancevd <olamosob@gmail.com>
2024-06-01 08:06:38 +0100

    Navbar settled

--- components/Common/Nav/NavItem.jsx
+++ components/Common/Nav/NavItem.jsx
@@ -5,18 +5,20 @@ import React from 'react'
 
 const NavItem = ({ NavIcon, NavText, NavRoute, setIsOpen }) => {
     const router = useRouter();
-    const className = router.asPath === `${NavRoute}` ? "rounded-xl !text-DeepNightBlack bg-Green font-bold tracking-widest" : '';
+    const className =
+      router.asPath === `${NavRoute}`
+        ? "rounded-xl !text-[#ffffff] font-bold tracking-widest"
+        : "";
 
     return (
-        <Link
-            onClick={(e) => setIsOpen(false)}
-            href={NavRoute}
-            className={`${className} transition flex items-center px-2 hover:bg-EveningBlack text-SilverGray hover:text-SilverGray rounded-xl  py-1.5 font-semibold space-x-4 text-base`}
-        >
-            {NavIcon}
-            <span>{NavText}</span>
-        </Link>
-    )
+      <Link
+        onClick={(e) => setIsOpen(false)}
+        href={NavRoute}
+        className={`${className} flex items-center px-2 hover:text-[#eeeeee] text-[#78787C]  rounded-xl  py-1.5 font-semibold space-x-4 text-base`}
+      >
+        {NavText}
+      </Link>
+    );
 }
 
 export default NavItem
