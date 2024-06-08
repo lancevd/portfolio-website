58405d45031db36c533c72ad942e7213ea3dd04b
lancevd <olamosob@gmail.com>
2024-06-08 10:43:28 +0100

    portfolio page set up

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,39 +1,98 @@
 import { useState } from "react";
-import { useQuery } from "react-query";
+import ProjectCard from "../components/Portfolio/ProjectCard";
+import { AnimatePresence, motion } from "framer-motion";
 import BannerLayout from "../components/Common/BannerLayout";
-import Footer from "../components/Footer";
-import PortfolioCard from "../components/Portfolio/PortfolioCard";
-import axios from "axios";
-import { Skeleton } from "antd";
-import ImageAndParagraphSkeleton from "../components/Common/ImageAndParagraphSkeleton";
+
+let projects = [
+  {
+    name: "KedvisMart",
+    image: "/images/chatter.png",
+    description:
+      "KedvisMart is an AI-assited ecommerce platform, where you can perform operations with voice commands to make accessibility easier",
+    link: "/project1",
+    category: "frontend development",
+  },
+  {
+    name: "Chatter App",
+    image: "/images/chatter.png",
+    description:
+      "Chatter is a mini blogging app, where you can create contents, interacts with posts, drop comments, and meet new people",
+    link: "/project1",
+    category: "frontend development",
+  },
+
+  {
+    name: "MovRent",
+    image: "/images/movrent.png",
+    description:
+      "Movrent is the place for movies enthusiasts, where they can check upcoming and latest movies, read plots, and view trailers",
+    link: "/project2",
+    category: "frontend development",
+  },
+  {
+    name: "Ticketbay",
+    image: "/images/ticketbay.png",
+    description:
+      "Ticketbay is a major ticketing platform in Nigeria. Organizers can create events, sell tickets, promote their events, and manage events.",
+    link: "/project3",
+    category: "Websites",
+  },
+  {
+    name: "Vue Github",
+    image: "/images/vue-github.png",
+    description: "Search for git hub profiles and display their repositories!",
+    link: "/project4",
+    category: "frontend development",
+  },
+  // Add
+];
+
+const categories = ["ALL", "WEBSITES", "FRONTEND DEVELOPMENT"];
 
 const Portfolio = () => {
+  const [selectedCategory, setSelectedCategory] = useState("ALL");
 
-    const { isLoading, error, data } = useQuery('portfolio', () =>
-        axios.get('api/portfolio')
-            .then(({ data }) => data)
-            .catch(error => console.error('Error fetching testimonials:', error)))
-    return (
-        <BannerLayout>
-            <div className="grid justify items-center grid-flow-row md:grid-cols-2 grid-rows-auto gap-4 px-8 my-6">
+  const handleFilter = (category) => {
+    setSelectedCategory(category);
+  };
 
-                {
-                    isLoading ?
-                        [1, 2, 3, 4].map(() => (
-                            <ImageAndParagraphSkeleton className={"w-full object-cover"} />
-                        ))
-                        :
-                        data?.map((data, key) => (
-                            <PortfolioCard key={key} data={data} />
-                        ))
+  const filteredProjects =
+    selectedCategory === "ALL"
+      ? projects
+      : projects.filter(
+          (project) =>
+            project.category.toLowerCase() === selectedCategory.toLowerCase()
+        );
 
-                }
-
-
-            </div >
-            <Footer />
-        </BannerLayout >
-    );
+  return (
+    <BannerLayout>
+      <div className="p-8">
+        <h1 className="text-4xl font-bold text-white mb-4">Portfolio</h1>
+        <div className="flex space-x-4 mb-4">
+          {categories.map((category, index) => (
+            <button
+              key={index}
+              className={`text-[#78787C] text-sm ${
+                selectedCategory === category
+                  ? "font-bold text-white"
+                  : "font-semibold"
+              }`}
+              onClick={() => handleFilter(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <AnimatePresence>
+            {filteredProjects.map((project, index) => (
+              <ProjectCard key={index} project={project} />
+            ))}
+          </AnimatePresence>
+        </div>
+      </div>
+    </BannerLayout>
+  );
 };
 
 export default Portfolio;
