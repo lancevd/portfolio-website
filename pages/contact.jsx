e5b43c797335282f692a9abacf3a860c5f76d9e7
lancevd <olamosob@gmail.com>
2024-06-08 12:45:09 +0100

    Contact through

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,120 +1,224 @@
-import { useState } from 'react';
-import BannerLayout from '../components/Common/BannerLayout';
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
-import { SiUpwork } from 'react-icons/si'
-import { HiMail, HiUser } from 'react-icons/hi'
-import { BsChatTextFill } from 'react-icons/bs'
-import Fiverr_Icon from '../components/Fiverr_Icon';
-import Footer from '../components/Footer';
-import { Modal } from 'antd';
+import { useState } from "react";
+import BannerLayout from "../components/Common/BannerLayout";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { HiMail, HiUser } from "react-icons/hi";
+import { BsChatTextFill } from "react-icons/bs";
+import Footer from "../components/Footer";
+import { Modal } from "antd";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-    return (
-        <BannerLayout>
-            <div className=" px-4 py-2">
-                <div className="my-6 text-Snow flex flex-col gap-y-5">
-                    <h1 className='text-lg font-bold'>Contact Information</h1>
-                    <div className="flex flex-col md:flex-row items-center gap-5 text-xs">
-                        <div className="card_stylings w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
-                            <div className="flex justify-between items-center">
-                                <span className='md:text-base'>Country:</span>
-                                <span className='text-LightGray md:text-sm'>Pakistan</span>
-                            </div>
-                            <div className="flex justify-between items-center">
-                                <span className='md:text-base'>City:</span>
-                                <span className='text-LightGray md:text-sm'>Peshawar</span>
-                            </div>
-                            <div className="flex justify-between items-center">
-                                <span className='md:text-base'>Company:</span>
-                                <span className='text-LightGray md:text-sm'>visualsX</span>
-                            </div>
-                        </div>
-                        <div className="card_stylings rounded-xl w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
-                            <div className="flex justify-between items-center">
-                                <span className='md:text-base'>Email:</span>
-                                <span className='text-LightGray text-sm'>iosamajavaid@gmail.com</span>
-                            </div>
-                            <div className="flex justify-between items-center">
-                                <span className='md:text-base'>Linkedin:</span>
-                                <span className='text-LightGray text-sm'>iosamajavaid</span>
-                            </div>
-                            <div className="flex justify-between items-center">
-                                <span className='md:text-base'>Phone:</span>
-                                <span className='text-LightGray text-sm'>+92 (315) 9591822</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="h-16 w-full card_stylings text-xl sm:text-3xl flex gap-x-8 sm:gap-x-16 items-center justify-center text-Snow">
-                    <a className='hover:scale-125 ease-in-out duration-700' href="" target='_blank' rel="noreferrer"><HiMail /></a>
-                    <a className='hover:scale-125 ease-in-out duration-700' href="https://github.com/osamajavaid" target='_blank' rel="noreferrer"><FaGithub /></a>
-                    <a className='hover:scale-125 ease-in-out duration-700' href="https://www.linkedin.com/in/iosamajavaid/" target='_blank' rel="noreferrer"><FaLinkedin /></a>
-                    {/* <a className='hover:scale-125 ease-in-out duration-700' href="https://x.com/iosamajavaid" target='_blank' rel="noreferrer"><FaTwitter /></a> */}
-                    <a className='hover:scale-125 ease-in-out duration-700 hidden sm:block' href="https://www.fiverr.com/codeworthy" target='_blank' rel="noreferrer"><Fiverr_Icon /></a>
-                    <a className='hover:scale-125 ease-in-out duration-700 text-2xl sm:text-4xl mt-1' href="#" target='_blank' rel="noreferrer"><SiUpwork /></a>
-                </div>
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                <div className="my-12 w-full h-auto text-Snow">
-                    <h1 className='text-lg font-bold'>Get In Touch</h1>
-                    <div className="mt-4 py-8 px-8 bg-EveningBlack rounded-xl text-sm">
-                        <div>
-                            <div className="flex flex-col w-full">
-                                <div className="userIcon relative mb-6">
-                                    <div id="icon" className="absolute inset-y-0 left-0 flex items-center pl-3 text-xl pointer-events-none">
-                                        <HiUser />
-                                    </div>
-                                    <input type="text" className="input_stylings" placeholder="Name" />
-                                </div>
-                            </div>
+    emailjs
+      .send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", formData, "YOUR_USER_ID")
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          setIsOpen(true); // Show success modal
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          alert("Error sending message. Please try again later.")
+        }
+      );
 
-                            <div className="flex flex-col w-full">
-                                <div className="mailIcon relative mb-6">
-                                    <div id="icon" className="absolute inset-y-0 left-0 flex items-center text-xl pl-3 pointer-events-none">
-                                        <HiMail />
-                                    </div>
-                                    <input type="text" className="input_stylings" placeholder="Email" />
-                                </div>
-                            </div>
+    // Reset form fields
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
 
-                            <div className="flex flex-col w-full">
-                                <div className="textIcon relative mb-6">
-                                    <div id="icon" className="absolute top-3 left-0 flex items-center text-lg pl-3 pointer-events-none">
-                                        <BsChatTextFill />
-                                    </div>
-                                    <textarea rows={6} cols={50} className="input_stylings" placeholder="Message" />
-                                </div>
-                            </div>
-
-                            <div className="my-4">
-                                <button onClick={() => setIsOpen(true)} className="button"> SEND MESSAGE </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <BannerLayout>
+      <div className=" px-4 py-2">
+        <div className="my-6 text-Snow flex flex-col gap-y-5">
+          <h1 className="text-lg font-bold">Contact Information</h1>
+          <div className="flex flex-col md:flex-row items-center gap-5 text-xs">
+            <div className="card_stylings w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
+              <div className="flex justify-between items-center">
+                <span className="md:text-base">Country:</span>
+                <span className="text-LightGray md:text-sm">Nigeria</span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="md:text-base">State:</span>
+                <span className="text-LightGray md:text-sm">Kwara</span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="md:text-base">City:</span>
+                <span className="text-LightGray md:text-sm">Ilorin</span>
+              </div>
             </div>
-            {/* success modal */}
-            <Modal
-                className='card_stylings backdrop-blur-3xl drop-shadow-2xl'
-                // wrapClassName='bg-red-800'
-                centered
-                open={isOpen}
-                footer={null}
-                closable={false}
-                onOk={() => setIsOpen(false)}
-                onCancel={() => setIsOpen(false)}
-            >
-                <div className='flex flex-col items-center justify-center'>
-                    <h1 className='text-Green font-bold text-2xl'>In Progress</h1>
-                    <a className='underline text-Snow' target='_blank' href='https://github.com/osamajavaid/portfolio'>Be the one to integrate this!</a>
+            <div className="card_stylings rounded-xl w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
+              <div className="flex justify-between items-center">
+                <span className="md:text-base">Email:</span>
+                <span className="text-LightGray text-sm underline">
+                  <a href="mailto:olamosb@gmail.com" target="_blank">
+                    olamosob@gmail.com
+                  </a>
+                </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="md:text-base">Linkedin:</span>
+                <span className="text-LightGray text-sm underline">
+                  <a
+                    href="https://www.linkedin.com/in/winner-mosob"
+                    target="_blank"
+                  >
+                    winner-mosob
+                  </a>
+                </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="md:text-base">Phone:</span>
+                <span className="text-LightGray text-sm underline">
+                  <a href="tel:+2348142418465">+2348142418465</a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-16 w-full card_stylings text-xl sm:text-3xl flex gap-x-8 sm:gap-x-16 items-center justify-center text-Snow">
+          <a
+            className="hover:scale-125 ease-in-out duration-700"
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            <HiMail />
+          </a>
+          <a
+            className="hover:scale-125 ease-in-out duration-700"
+            href="https://github.com/lancevd"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            className="hover:scale-125 ease-in-out duration-700"
+            href="https://www.linkedin.com/in/winner-mosob/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            className="hover:scale-125 ease-in-out duration-700"
+            href="https://x.com/lance4vd"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitter />
+          </a>
+        </div>
+
+        <div className="my-12 w-full h-auto text-Snow">
+          <h1 className="text-lg font-bold">Get In Touch</h1>
+          <div className="mt-4 py-8 px-8 bg-EveningBlack rounded-xl text-sm">
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col w-full">
+                <div className="userIcon relative mb-6">
+                  <div
+                    id="icon"
+                    className="absolute inset-y-0 left-0 flex items-center pl-3 text-xl pointer-events-none"
+                  >
+                    <HiUser />
+                  </div>
+                  <input
+                    required
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    className="input_stylings"
+                    placeholder="Name"
+                  />
                 </div>
-            </Modal>
-            <Footer />
-        </BannerLayout>
+              </div>
 
-    )
-}
+              <div className="flex flex-col w-full">
+                <div className="mailIcon relative mb-6">
+                  <div
+                    id="icon"
+                    className="absolute inset-y-0 left-0 flex items-center text-xl pl-3 pointer-events-none"
+                  >
+                    <HiMail />
+                  </div>
+                  <input
+                    required
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    className="input_stylings"
+                    placeholder="Email"
+                  />
+                </div>
+              </div>
 
-export default Contact
+              <div className="flex flex-col w-full">
+                <div className="textIcon relative mb-6">
+                  <div
+                    id="icon"
+                    className="absolute top-3 left-0 flex items-center text-lg pl-3 pointer-events-none"
+                  >
+                    <BsChatTextFill />
+                  </div>
+                  <textarea
+                    required
+                    rows={6}
+                    cols={50}
+                    name="message"
+                    value={formData.message}
+                    onChange={handleInputChange}
+                    className="input_stylings"
+                    placeholder="Message"
+                  />
+                </div>
+              </div>
+
+              <div className="my-4">
+                <button type="submit" className="button">
+                  SEND MESSAGE
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* success modal */}
+      <Modal
+        className="card_stylings backdrop-blur-3xl drop-shadow-2xl"
+        centered
+        open={isOpen}
+        footer={null}
+        closable={false}
+        onOk={() => setIsOpen(false)}
+        onCancel={() => setIsOpen(false)}
+      >
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-[#5CB4F0] font-bold text-2xl">Message Sent</h1>
+        </div>
+      </Modal>
+      <Footer />
+    </BannerLayout>
+  );
+};
+
+export default Contact;
