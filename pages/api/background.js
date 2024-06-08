73bb659a648fc93e18213f6327f938fa6855be15
lancevd <olamosob@gmail.com>
2024-06-08 13:18:25 +0100

    first run through

--- pages/api/background.js
+++ pages/api/background.js
@@ -1,64 +1,74 @@
 const background = [
-    {
-        eduCards: [
-            {
-                id: 0,
-                title: 'UET Peshawar',
-                degree: 'BS, Computer System Engineering',
-                detail: "Bachelor's Degree in Computer System Engineering from UET Peshawar.",
-                year: '2018-2022'
-            },
-            {
-                id: 1,
-                title: 'Peshawar Model Degree College',
-                degree: 'HSSC, Pre Engineering',
-                detail: "Completed FSC part 1 and part 2 in Pre-Engineering from Peshawar Model Degree College.",
-                year: '2015-2017'
-            },
-            {
-                id: 2,
-                title: 'Falcon Academy',
-                degree: 'SSC, Science Subjects',
-                detail: "Completed SSC part 1 and part 2 in Science subjects from Falcon Academy Seni Gumbat Kohat",
-                year: '2013-2015'
-            },
-        ]
-    },
-    {
-        expCards: [
-            {
-                id: 1,
-                title: 'JMM Technologies',
-                role: 'Frontend Developer',
-                url: 'https://jmm.ltd/',
-                desc: 'As a frontend developer, I use React, Next & JavaScript to build user interfaces for web applications.',
-                year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
-            },
-            {
-                id: 2,
-                title: 'HauzaTech',
-                role: 'Internee',
-                url: 'no website',
-                desc: 'As an Internee, I learned how to use React & JavaScript to build interactive websites.',
-                year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
-            },
-            {
-                id: 3,
-                title: 'Encoder Bytes',
-                role: 'PHP Developer',
-                url: 'https://www.encoderbytes.com/',
-                desc: "I work there as a PHP developer, there I learned how to do CRUD'S operations in PHP, also I worked on Firebase",
-                year: '09/2020 - 02/2021',
-                location: 'Peshawar, Pakistan'
-            },
-
-        ]
-    }
-]
-
+  {
+    eduCards: [
+      {
+        id: 0,
+        title: "University of Ilorin",
+        degree: "BSc, Agricultural & Biosystems Engineering",
+        detail:
+          "Bachelor's Degree in Agricultural & Biosystems Engineering, specialized in Power and Machinery.",
+        year: "2013-2019",
+      },
+      {
+        id: 1,
+        title: "AltSchool Africa",
+        degree: "Diploma, Software Engineering",
+        detail: "Completed a Diploma in Frontend Engineering.",
+        year: "2021-2022",
+      },
+    ],
+  },
+  {
+    expCards: [
+      {
+        id: 1,
+        title: "Bare Mind Inc.",
+        role: "Frontend Developer",
+        url: "https://www.bare-mind.com/",
+        desc: "Leads a team of developers, working on Bare Mind, a mental health/wellness platform.",
+        year: "09/2021 - Present",
+        location: "Johannesburg, South Africa",
+      },
+      {
+        id: 2,
+        title: "MSMEs Ecosystems (Tulupay)",
+        role: "Frontend Developer",
+        url: "",
+        desc: "Led a team of frontend developers on a Tulupay, an african-based fintech platform.",
+        year: "09/2023 - 12/2023",
+        location: "Ilorin, Nigeria",
+      },
+      {
+        id: 3,
+        title: "Pacifylabs Technologies",
+        role: "Frontend Developer",
+        url: "https://pacifylabs.tech/",
+        desc: "Worked as a frontend developer. Worked on various projects mainly with React.js and Next.js",
+        year: "02/2022 - 05/2023",
+        location: "Ilorin, Nigeria",
+      },
+      {
+        id: 4,
+        title: "Ticketbay",
+        role: "Frontend/Web Developer",
+        url: "https://ticketbay.com.ng/",
+        desc: "Built a ticketing website for the company. Worked as frontend developer on a custom ticketing solution.",
+        year: "04/2020 - 12/2021",
+        location: "Lagos, Nigeria",
+      },
+      {
+        id: 5,
+        title: "Upwork & Fiverr",
+        role: "Freelance Web Developer (Wordpress)",
+        url: "",
+        desc: "Developed wordpress websites for differents clients on freelancing platforms",
+        year: "05/2019 - 01/2022",
+        location: "",
+      },
+    ],
+  },
+];
 
 export default function handler(req, res) {
-    res.status(200).json(background)
+  res.status(200).json(background);
 }
--- pages/experience.jsx
+++ pages/experience.jsx
@@ -16,40 +16,42 @@ function Background() {
             .catch(error => console.error('Error fetching testimonials:', error)))
 
     return (
-        <BannerLayout>
-            <div className="grid md:grid-cols-2 md:divide-x-4 md:divide-Green px-4 pb-2 pt-10">
-                <div className="flex flex-col gap-y-4 order-2 md:order-1  md:mr-12">
-                    <div className="mt-10 md:mt-0 text-xl text-Snow font-semibold">Education</div>
-                    {isLoading ?
-                        [1, 2, 3].map(() => (
-                            <ParagraphSkeleton className={"p-8 h-full w-full relative"} />
-                        ))
-                        :
-                        data && data[0]?.eduCards?.map((data, key) => (
-                            <Edu_Card key={key} data={data} />
-                        ))
-                    }
-
-                </div>
-                <div className="order-1 md:order-2">
-                    <div className="flex flex-col gap-y-4 md:ml-12">
-                        <div className=" md:pt-0 pt-4 text-xl text-Snow font-semibold">Experience</div>
-
-                        {isLoading ?
-                            [1, 2, 3].map(() => (
-                                <ParagraphSkeleton className={"p-8 h-full w-full relative"} />
-                            ))
-                            :
-                            data && data[1]?.expCards?.map((data, key) => (
-                                <Exp_Card key={key} data={data} />
-                            ))
-                        }
-
-                    </div>
-                </div>
+      <BannerLayout>
+        <div className="grid md:grid-cols-2 md:divide-x-4 md:divide-[#5CB4F0] px-4 pb-2 pt-10">
+          <div className="flex flex-col gap-y-4 order-2 md:order-1  md:mr-12">
+            <div className="mt-10 md:mt-0 text-xl text-Snow font-semibold">
+              Education
             </div>
-            <Footer />
-        </BannerLayout>
+            {isLoading
+              ? [1, 2, 3].map(() => (
+                  <ParagraphSkeleton className={"p-8 h-full w-full relative"} />
+                ))
+              : data &&
+                data[0]?.eduCards?.map((data, key) => (
+                  <Edu_Card key={key} data={data} />
+                ))}
+          </div>
+          <div className="order-1 md:order-2">
+            <div className="flex flex-col gap-y-4 md:ml-12">
+              <div className=" md:pt-0 pt-4 text-xl text-Snow font-semibold">
+                Experience
+              </div>
+
+              {isLoading
+                ? [1, 2, 3].map(() => (
+                    <ParagraphSkeleton
+                      className={"p-8 h-full w-full relative"}
+                    />
+                  ))
+                : data &&
+                  data[1]?.expCards?.map((data, key) => (
+                    <Exp_Card key={key} data={data} />
+                  ))}
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </BannerLayout>
     );
 }
 
