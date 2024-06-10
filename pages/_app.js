855220ce6ea0618cabaaafd64ebf7fb3b974704c
lancevd <olamosob@gmail.com>
2024-06-10 08:05:25 +0100

    Meta Fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 import Layout from "./layout";
@@ -5,19 +6,24 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-export const metadata = {
-  title: "Olamide Mosoobalaje",
-  description:
-    "Olamide Mosobalaje, an experienced Frontend Developer based in Nigeria",
-  author: "Olamide Winner Mosobalaje",
-  keywords:
-    "Frontend Developer, frontend engineer, african frontend developer, african frontend engineer, frontend developer in nigeria",
-  favicon: "/favicon.png",
-};
-
 export default function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Olamide Mosoobalaje | Frontend Engineer</title>
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+        <meta name="image" content="/favicon.png" />
+        <meta
+          name="description"
+          content="Olamide Mosobalaje, an experienced Frontend Developer based in Nigeria"
+        />
+        <meta
+          name="keywords"
+          content="Frontend Developer, frontend engineer, african frontend developer, african frontend engineer, frontend developer in nigeria, React developer, next.js developer, react developer nigeria"
+        />
+        <meta name="author" content="Olamide Mosobalaje" />
+        <meta name="title" content="Olamide Mosobalaje | Frontend Developer" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
