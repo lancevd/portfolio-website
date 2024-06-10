d9cce0813a6e38f813a8d6937da5ebb74c7b7c37
lancevd <olamosob@gmail.com>
2024-06-10 07:54:09 +0100

    meta added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import Layout from './layout'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import Layout from "./layout";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
+export const metadata = {
+  title: "Olamide Mosoobalaje",
+  description:
+    "Olamide Mosobalaje, an experienced Frontend Developer based in Nigeria",
+  author: "Olamide Winner Mosobalaje",
+  keywords:
+    "Frontend Developer, frontend engineer, african frontend developer, african frontend engineer, frontend developer in nigeria",
+  favicon: "/favicon.png",
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,5 +22,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
     </QueryClientProvider>
-  )
+  );
 }
